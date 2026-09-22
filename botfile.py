14f1922cca9ece7cf4c89f3a8a76dbb1f2518c99
sheteharsh27-lgtm <sheteharsh27@gmail.com>
2026-09-22 10:47:03 +0530

    Update botfile.py

--- botfile.py
+++ botfile.py
@@ -1,14 +1,9 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
 import os
 
-BOT_TOKEN = os.getenv("8720188300:AAGC-OQPQSkwXeIz5Jlx0iN3thF3K_GmQCI")
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is working!")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 def main():
-    app = Application.builder().token(8720188300:AAGC-OQPQSkwXeIz5Jlx0iN3thF3K_GmQCI).build()
+    app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
