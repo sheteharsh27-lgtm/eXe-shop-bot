b81cda7053d6175154373b2ac57c8d9141814de4
sheteharsh27-lgtm <sheteharsh27@gmail.com>
2026-09-22 10:36:24 +0530

    Update botfile.py

--- botfile.py
+++ botfile.py
@@ -1,5 +1,19 @@
-eXe-shop-bot/
-├── bot.py
-├── requirements.txt
-├── .gitignore
-└── README.md
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot is working!")
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot started...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
